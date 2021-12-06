c315e4132974dad2ae9e1fc3885f9ed9e28c8818
AshwiniChin <76168179+AshwiniChin@users.noreply.github.com>
2021-12-06 10:10:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+function makeBlur() {
+    console.log("making image blur");
+    img = document.getElementById("image1");
+    document.getElementById("result").innerHTML = "<img src='" + img.src + "'>";
+    document.getElementById("result").style.filter = "blur(2px)";
+}
+
+function editBlackWhite() {
+    console.log("making image black & white");
+    img = document.getElementById("image1");
+    document.getElementById("result").innerHTML = "<img src='" + img.src + "'>";
+    document.getElementById("result").style.filter = "grayscale()";
+}
+
+function editSepia() {
+    console.log("making image editSepia");
+    img = document.getElementById("image1");
+    document.getElementById("result").innerHTML = "<img src='" + img.src + "'>";
+    document.getElementById("result").style.filter = "sepia()";
+}
+
+function editInvert() {
+    console.log("making image invert colors");
+    img = document.getElementById("image1");
+    document.getElementById("result").style.filter = "invert()";
+    document.getElementById("result").innerHTML = "<img src='" + img.src + "'>";
+   
+}
+
+
+
